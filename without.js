29604df44bb4760a563ea215ee53d4988d691e66
Julian Bustos <julian.m.bustos@gmail.com>
2020-05-27 01:13:24 +0000

    remove non equal elements of arrays being compared

---
+++ without.js
@@ -0,0 +1,37 @@
+//implement assertArraysEqual with 2 inputs 
+
+const assertArraysEqual = function(arr1, arr2){
+  for (let i = 0; i < arr1.length; i++) {
+  assertEqual(arr1[i], arr2[i]);
+  };
+  return 
+}; 
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👌 Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`😖 Assertion Failed: ${actual} !== ${expected}`)
+  }
+  };
+
+  const eqArrays = function(arr1, arr2) { 
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return [false];
+    }
+  }
+   return [true];
+  };
+
+  const without = function(arr1, arr2) {
+    let noMatch = []
+    for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      noMatch.push(arr1[i]);
+      return noMatch
+    }
+  }
+  }
+
+assertArraysEqual(without(['hi', 'hello'], ['hi']), ['hello', 'what'])
