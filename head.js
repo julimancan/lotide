f9822e49edf3acec920c7cc6ec050174831fbccd
Julian Bustos <julian.m.bustos@gmail.com>
2020-06-01 18:12:38 +0000

    added assertEqual through require instead of copy paste

--- head.js
+++ head.js
@@ -1,16 +1,12 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(input) {
     let h = input[0];
     return h;
   }
   
   
-  const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👌 Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`😖 Assertion Failed: " ${actual} !== ${expected}`)
-  }
-  };
+
   
   assertEqual(head([5,6,7]), 5);
   assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
