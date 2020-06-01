93a73dd747dd994eb12e4737c50db2d8b0c3958f
Julian Bustos <julian.m.bustos@gmail.com>
2020-06-01 23:01:04 +0000

    fixed errors and added require and export

--- test/assertObjectsEqual.test.js
+++ test/assertObjectsEqual.test.js
@@ -1,4 +1,5 @@
 const assertObjectsEqual = require("../assertObjectsEqual");
+const chalk = require('chalk');
 
 
 
@@ -6,5 +7,5 @@ const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 
 
-console.log('assertObjectsEqual:');
+console.log(chalk.blue('assertObjectsEqual:'));
 assertObjectsEqual(ab, ba);
